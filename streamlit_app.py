c7631f7de3908169529f6c3ca3d1437b33d0e46c
mclean-snowflake-training <149437911+mclean-snowflake-training@users.noreply.github.com>
2023-10-30 16:11:56 -0700

    Update streamlit_app.py
    smoothie header

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,7 +4,9 @@ streamlit.title('My Mom\'s New Healthy Diner')
 
 streamlit.header('Breakfast Menu')
 
-streamlit.text('🥣Omega 3 & Blueberry Oatmeal')
-streamlit.text('🥗Kale, Spinach & Rocket Smoothie')
-streamlit.text('🐔Hard-Boiled Free-Range Egg')
-streamlit.text('🥑🍞Avocado Toast')
+streamlit.text('🥣 Omega 3 & Blueberry Oatmeal')
+streamlit.text('🥗 Kale, Spinach & Rocket Smoothie')
+streamlit.text('🐔 Hard-Boiled Free-Range Egg')
+streamlit.text('🥑🍞 Avocado Toast')
+
+streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
